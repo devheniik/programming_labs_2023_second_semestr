8c673e2f4810645bd466f7921ac6b27fc1b26619
Yevhenii Kyrmyzy <yevhenii_kyrmyzy@icloud.com>
2023-03-15 03:55:14 +0200

    add first laba

---
+++ laba1/laba1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+class Program {
+
+    static void FirstTask()
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 2, 1, 4, 5, 4, 6 };
+        Dictionary<int, int> frequency = new Dictionary<int, int>();
+
+        
+        // Counting the number of occurrences of each element
+        foreach (int number in numbers) {
+            if (frequency.ContainsKey(number)) {
+                frequency[number]++;
+            }
+            else {
+                frequency[number] = 1;
+            }
+        }
+
+        int pairsCount = 0;
+
+        // Counting the number of pairs of elements equal to one to one for example: 1 occurrences 5 times it will be 2 pairs
+        foreach (int value in frequency.Values) {
+            pairsCount += value / 2;
+        }
+
+        Console.WriteLine($"Number of pairs of elements equal to one to one: {pairsCount}");
+    }
+
+    static void SecondTask()
+    {
+        Dictionary<string, int> dict = new Dictionary<string, int> {
+            { "key1", 3 },
+            { "key2", 2 },
+            { "key3", 4 },
+            { "key4", 1 }
+        };
+
+        int product = 1;
+        foreach (int value in dict.Values) {
+            product *= value;
+        }
+
+        dict = dict.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+        Console.WriteLine("Sorted dictionary in JSON:");
+        Console.WriteLine(JsonSerializer.Serialize(dict));
+
+        Console.WriteLine($"Product of keys: {product}");
+    }
+    
+    static void ThirdTask()
+    {
+        char c = 'a';
+        string[] A = { "abc", "def", "agca", "caba", "acca", "acc", "a", "aa" };
+
+        int count = A.Where(x => x.Length > 1 && x.StartsWith(c) && x.EndsWith(c)).Count();
+
+        Console.WriteLine($"Quantity of A elements, which contain 1+ symbol C and starts by C and ends by C: {count}");
+    }
+    
+    static void Main(string[] args) {
+        // FirstTask();
+        // SecondTask();
+        // ThirdTask();
+    }
+}
---
+++ laba3/laba3/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace ComplexNumber
+{
+    public class Complex
+    {
+        private double _real;
+        private double _imaginary;
+
+        public Complex(double real, double imaginary)
+        {
+            _real = real;
+            _imaginary = imaginary;
+        }
+
+        // Геттери
+        public double GetReal()
+        {
+            return _real;
+        }
+
+        public double GetImaginary()
+        {
+            return _imaginary;
+        }
+
+        
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a._real + b._real, a._imaginary + b._imaginary);
+        }
+
+        
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a._real - b._real, a._imaginary - b._imaginary);
+        }
+
+        
+        public static Complex operator *(Complex a, Complex b)
+        {
+            double real = a._real * b._real - a._imaginary * b._imaginary;
+            double imaginary = a._real * b._imaginary + a._imaginary * b._real;
+            return new Complex(real, imaginary);
+        }
+
+        
+        public static Complex operator /(Complex a, Complex b)
+        {
+            double denominator = b._real * b._real + b._imaginary * b._imaginary;
+            double real = (a._real * b._real + a._imaginary * b._imaginary) / denominator;
+            double imaginary = (a._imaginary * b._real - a._real * b._imaginary) / denominator;
+            return new Complex(real, imaginary);
+        }
+
+        
+        public double GetMagnitude()
+        {
+            return Math.Sqrt(_real * _real + _imaginary * _imaginary);
+        }
+
+        
+        public double GetArgument()
+        {
+            return Math.Atan2(_imaginary, _real);
+        }
+ 
+        public Complex ToTrigonometricForm()
+        {
+            double magnitude = GetMagnitude();
+            double argument = GetArgument();
+            return new Complex(magnitude * Math.Cos(argument), magnitude * Math.Sin(argument));
+        }
+ 
+        public Complex ToExponentialForm()
+        {
+            double magnitude = GetMagnitude();
+            double argument = GetArgument();
+            return new Complex(magnitude * Math.Exp(argument), 0);
+        }
+ 
+        public Complex Pow(double power)
+        {
+            double magnitude = Math.Pow(GetMagnitude(), power);
+            double argument = GetArgument() * power;
+            return new Complex(magnitude * Math.Cos(argument), magnitude * Math.Sin(argument));
+        }
+ 
+        public Complex[] Sqrt(int n)
+        {
+            Complex[] roots = new Complex[n];
+            double magnitude = Math.Pow(GetMagnitude(), 1.0 / n);
+            double phi = GetArgument();
+            for (int i = 0; i < n; i++)
+            {
+                double real = magnitude * Math.Cos((phi + 2 * Math.PI * i) / n);
+                double imaginary = magnitude * Math.Sin((phi + 2 * Math.PI * i) / n);
+                roots[i] = new Complex(real, imaginary);
+            }
+            return roots;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex z1 = new Complex(3, 4);
+            Complex z2 = new Complex(-2, 1);
+
+            Complex sum = z1 + z2;
+            Console.WriteLine($"Sum: ({sum.GetReal()}, {sum.GetImaginary()})");
+
+            Complex difference = z1 - z2;
+            Console.WriteLine($"Difference: ({difference.GetReal()}, {difference.GetImaginary()})");
+
+            Complex product = z1 * z2;
+            Console.WriteLine($"Product: ({product.GetReal()}, {product.GetImaginary()})");
+
+            Complex quotient = z1 / z2;
+            Console.WriteLine($"Quotient: ({quotient.GetReal()}, {quotient.GetImaginary()})");
+
+            Complex power = z1.Pow(2);
+            Console.WriteLine($"Z1 squared: ({power.GetReal()}, {power.GetImaginary()})");
+
+            Complex[] roots = z1.Sqrt(3);
+            Console.WriteLine($"Z1 cube roots:");
+            foreach (Complex root in roots)
+            {
+                Console.WriteLine($"({root.GetReal()}, {root.GetImaginary()})");
+            }
+
+            Complex trigonometricForm = z1.ToTrigonometricForm();
+            Console.WriteLine($"Trigonometric form of z1: ({trigonometricForm.GetReal()}, {trigonometricForm.GetImaginary()})");
+
+            Complex exponentialForm = z1.ToExponentialForm();
+            Console.WriteLine($"Exponential form of z1: ({exponentialForm.GetReal()}, {exponentialForm.GetImaginary()})");
+        }
+    }
+}   
