d2e8fea34ef418e413ea800c5fc8342833d28182
Yevhenii Kyrmyzy <yevhenii_kyrmyzy@icloud.com>
2023-05-11 17:32:41 +0300

    feat: add 5

---
+++ laba5.1.0/laba5.1.0/Program.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Programm
+{
+    public abstract class Vessel
+    {
+        public int Speed { get; set; }
+        public abstract int CarryingCapacity { get; }
+        public abstract string FuelType { get; }
+
+        public void Move()
+        {
+            Console.WriteLine("Moving at {0} knots...", Speed);
+        }
+        
+        public void Info()
+        {
+            Console.WriteLine("This vessel can carry {0} tons and uses {1} as fuel.", CarryingCapacity,
+                FuelType);
+
+        }
+    }
+
+    public class CargoShip : Vessel
+    {
+        public override int CarryingCapacity
+        {
+            get { return 5000; }
+        }
+
+        public override string FuelType
+        {
+            get { return "Diesel"; }
+        }
+    }
+
+    public class Tanker : Vessel
+    {
+        public override int CarryingCapacity
+        {
+            get { return 10000; }
+        }
+
+        public override string FuelType
+        {
+            get { return "Heavy Fuel Oil"; }
+        }
+    }
+
+    public class ContainerCarrier : Vessel
+    {
+        public override int CarryingCapacity
+        {
+            get { return 2000; }
+        }
+
+        public override string FuelType
+        {
+            get { return "LNG"; }
+        }
+    }
+
+    public class Boat : Vessel
+    {
+        public override int CarryingCapacity
+        {
+            get { return 10; }
+        }
+
+        public override string FuelType
+        {
+            get { return "Gasoline"; }
+        }
+    }
+
+    public class Canoe : Vessel
+    {
+        public override int CarryingCapacity
+        {
+            get { return 1; }
+        }
+
+        public override string FuelType
+        {
+            get { return "Muscle Power"; }
+        }
+    }
+
+// Example usage
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vessel myVessel = new CargoShip();
+            myVessel.Speed = 20;
+            myVessel.Move();
+            myVessel.Info();
+
+            myVessel = new Canoe();
+            myVessel.Speed = 5;
+            myVessel.Move();
+            myVessel.Info();
+        }
+    }
+}
---
+++ laba5.2.0/laba5.2.0/Program.cs
@@ -0,0 +1,158 @@
+﻿namespace Programm
+{
+    public interface IVessel
+    {
+        int Speed { get; set; }
+        int CarryingCapacity { get; }
+        string FuelType { get; }
+        void Move();
+        void Info();
+    }
+
+    public class CargoShip : IVessel
+    {
+        public int Speed { get; set; }
+
+        public int CarryingCapacity
+        {
+            get { return 5000; }
+        }
+
+        public string FuelType
+        {
+            get { return "Diesel"; }
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Moving at {0} knots...", Speed);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("This vessel can carry {0} tons and uses {1} as fuel.", CarryingCapacity,
+                FuelType);
+        }
+    }
+
+    public class Tanker : IVessel
+    {
+        public int Speed { get; set; }
+
+        public int CarryingCapacity
+        {
+            get { return 10000; }
+        }
+
+        public string FuelType
+        {
+            get { return "Heavy Fuel Oil"; }
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Moving at {0} knots...", Speed);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("This vessel can carry {0} tons and uses {1} as fuel.", CarryingCapacity,
+                FuelType);
+        }
+    }
+
+    public class ContainerCarrier : IVessel
+    {
+        public int Speed { get; set; }
+
+        public int CarryingCapacity
+        {
+            get { return 2000; }
+        }
+
+        public string FuelType
+        {
+            get { return "LNG"; }
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Moving at {0} knots...", Speed);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("This vessel can carry {0} tons and uses {1} as fuel.", CarryingCapacity,
+                FuelType);
+        }
+    }
+
+    public class Boat : IVessel
+    {
+        public int Speed { get; set; }
+
+        public int CarryingCapacity
+        {
+            get { return 10; }
+        }
+
+        public string FuelType
+        {
+            get { return "Gasoline"; }
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Moving at {0} knots...", Speed);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("This vessel can carry {0} tons and uses {1} as fuel.", CarryingCapacity,
+                FuelType);
+        }
+    }
+
+    public class Canoe : IVessel
+    {
+        public int Speed { get; set; }
+
+        public int CarryingCapacity
+        {
+            get { return 1; }
+        }
+
+        public string FuelType
+        {
+            get { return "Muscle Power"; }
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Moving at {0} knots...", Speed);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("This vessel can carry {0} tons and uses {1} as fuel.", CarryingCapacity,
+                FuelType);
+        }
+    }
+
+// Example usage
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IVessel myVessel = new CargoShip();
+            myVessel.Speed = 20;
+            myVessel.Move();
+            myVessel.Info();
+
+            myVessel = new Canoe();
+            myVessel.Speed = 5;
+            myVessel.Move();
+            myVessel.Info();
+        }
+    }
+}
